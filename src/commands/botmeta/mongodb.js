0e950741e96a6894f75e6b046f0a12b14de97c5f
Minnie A. Trethewey <minnietrethewey@gmail.com>
2026-01-25 11:36:28 -0800

    OOP: Access DB funcs from Discord

---
+++ src/commands/botmeta/mongodb.js
@@ -0,0 +1,80 @@
+// @ts-nocheck
+
+// Command Option Types
+const { ApplicationCommandOptionType, inlineCode, codeBlock, hyperlink } = require('discord.js')
+// BotDevCommand
+const { BotDevCommand } = require('../../classes/command/botdevcommand.class')
+
+const stringFuncs = require('../../utils/primitives/stringFuncs')
+const fileFuncs = require('../../utils/fs/fileFuncs')
+const dbFuncs = require('../../utils/db/dbFuncs')
+const shell = require('shelljs')
+const fs = require('fs')
+
+module.exports = class MongoDBCommand extends BotDevCommand {
+  constructor(client) {
+    let comprops = {
+      name: "mongodb",
+      category: "botmeta",
+      description: "Manage MongoDB's dbs for this guild",
+      flags: {
+        user: "unapplicable"
+      },
+      options: [
+        {
+          name: "mode",
+          description: "Mode",
+          type: ApplicationCommandOptionType.String
+        }
+      ]
+    }
+    let props = {}
+
+    super(
+      client,
+      {...comprops},
+      {...props}
+    )
+  }
+
+  // declare props: import('../../types/embed').EmbedProps
+
+  async action(client, interaction, coptions) {
+    this.props.playerTypes = {
+      user: "caller",
+      target: "bot"
+    }
+
+    // Get DB Type
+    let mode = coptions["mode"] ?? "ping"
+
+    let guild = interaction.guild
+
+    this.props.title = {
+      text: `MongoDB for ${guild.name}`
+    }
+
+    let mongodb_crun = null
+    try {
+      mongodb_crun = shell.exec(
+        `node ./src/mongodb/crun.js -m ${mode}`,
+        { silent: true }
+      )
+      mongodb_crun = mongodb_crun.stdout.trim()
+    } catch (err) {
+      console.log(err.stack)
+    }
+
+    // Bucket for console output
+    let console_output = [
+      "---"
+    ]
+
+    console_output.push(
+      ("\n" + codeBlock(mongodb_crun))
+    )
+    this.props.description = console_output
+
+    return !this.error
+  }
+}
